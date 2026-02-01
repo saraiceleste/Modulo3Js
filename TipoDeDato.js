f1afd80db9393164703ce2bea23e869e578e1fec
Sarai <saraimontes170@gmail.com>
2026-02-01 17:53:17 -0600

    first practice

---
+++ TipoDeDato.js
@@ -0,0 +1,29 @@
+let number = 42; 
+console.log(typeof number);
+
+let string = "veinticinco"
+console.log(typeof string);
+
+let NegativeNumber = -666;
+console.log(typeof NegativeNumber);
+
+let boolean = true;
+console.log(typeof boolean);
+
+let ZeroNumber = 0;
+console.log(typeof ZeroNumber);
+
+let espacio = "  ";
+console.log(typeof '');
+
+let nulo = null;
+console.log(typeof nulo);
+   /*'null' es un tipo de dato especial en JavaScript.
+Representa la ausencia de valor, su tipo es 'object'.
+*/
+
+let sinValor;
+console.log(typeof sinValor);
+
+let FalseData = false;
+console.log(typeof FalseData);
